bf7cd8d94b7d5b370106d9cad49c6071bb87ac3b
Joseph John Tombari <josephTombari@gmail.com>
2018-11-07 12:09:38 -0600

    cpp files made and subs added to them

--- card.cpp
+++ card.cpp
@@ -2,7 +2,45 @@
 #include <string>
 #include  "card.h"
 
+Card::Card(){
+
+};
+
+Card::Card(int rank, Suit s){
+  myRank = rank;
+  mySuit = s;
+
+};
+
 string Card::toString() const{
   return rankString(myRank) + suitString(mySuit);
-}
+};
+
+bool Card::sameSuitAs(const Card& c) const {
+
+};
+
+int  Card::getRank() const{
+  return myRank;
+};
+
+string Card::suitString(Suit s) const{
+
+
+};
+
+string Card::rankString(int r) const{
+
+};
+
+bool Card::operator == (const Card& rhs) const{
+
+
+};
+
+bool Card::operator != (const Card& rhs) const{
+
+
+};
+
 
--- deck.cpp
+++ deck.cpp
@@ -1,1 +1,17 @@
+#include  "deck.h"
 
+Deck::Deck(){
+
+};
+
+void Deck::shuffle(){
+
+};
+
+Card Deck::dealCard(){
+
+};
+
+int  Deck::size() const{
+
+};
