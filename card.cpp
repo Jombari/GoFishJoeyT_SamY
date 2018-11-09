3986e41acb43f09a2f4928aa07505fcf35b524b2
Samuel Yeboah <31531678+Samuel-Akwesi-Yeboah@users.noreply.github.com>
2018-11-09 13:00:06 -0600

    Finished

--- card.cpp
+++ card.cpp
@@ -1,75 +1,76 @@
+//
+// Created by Samuel Yeboah on 11/9/2018.
+//
 #include <iostream>
 #include <string>
 #include <random>
+#include "time.h"
 #include  "card.h"
 
 Card::Card(){
-  srand(time(0));
-
+    srand(time(0));
 };
 
 Card::Card(int rank, Suit s){
-  srand(time(0));
-  myRank = rank;
-  mySuit = s;
-
+    myRank = rank;
+    mySuit = s;
+    srand(time(0));
 };
 
 string Card::toString() const{
-  return rankString(myRank) + suitString(mySuit);
+    srand(time(0));
+    return rankString(myRank) + suitString(mySuit);
 };
 
 bool Card::sameSuitAs(const Card& c) const {
-  //bool test = (suitString(mySuit) == suitString(*(&c+ (int) sizeof(myRank))));
-  //return test;
-
+    srand(time(0));
+    if(c.mySuit == this->mySuit) {
+        return true;
+    }
+    return false;
 };
 
 int  Card::getRank() const{
-  return myRank;
+    srand(time(0));
+    return myRank;
 };
 
 string Card::suitString(Suit s) const{
-  switch(myRank){
-    case spades:
-      return "s";
+    srand(time(0));
+    switch(s){
+        case spades:
+            return "s";
 
-    case hearts:
-      return "h";
+        case hearts:
+            return "h";
 
-    case clubs:
-      return "c";
+        case clubs:
+            return "c";
 
-    case diamonds:
-      return "d";
-  }
+        case diamonds:
+            return "d";
+    }
 };
 
 string Card::rankString(int r) const{
+    srand(time(0));
+    if(r >1 && r < 11)
+        return  std::to_string(r);
 
-  if(r >1 && r < 11)
-    return  std::to_string(r);
+    switch(r){
+        case 1:
+            return "A";
+        case 11:
+            return "J";
+        case 12:
+            return "Q";
 
-  switch(r){
-    case 1:
-      return "A";
-    case 11:
-      return "J";
-    case 12:
-      return "Q";
-
-    case 13:
-      return "K";
-  }
+        case 13:
+            return "K";
+    }
 };
 
 bool Card::operator == (const Card& rhs) const{
-
+    srand(time(0));
+    return rhs.myRank == this->myRank && this->sameSuitAs(rhs);
 };
-
-bool Card::operator != (const Card& rhs) const{
-
-
-};
-
-
