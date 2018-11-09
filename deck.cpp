08be909e815cb33c8441b32031d49ba2257f01bc
Samuel Yeboah <31531678+Samuel-Akwesi-Yeboah@users.noreply.github.com>
2018-11-09 16:34:17 -0600

    Update deck.cpp

--- deck.cpp
+++ deck.cpp
@@ -1,62 +1,63 @@
-/
+//
 // Created by Samuel Yeboah on 11/9/2018.
 //
-#include "card.h"
-#include  "Deck.h"
+
+#include "player.h"
 using namespace std;
-Deck::Deck(){
-    Card::Suit cardType;
-    myIndex = 0;
-    int spot = 0;
-    for(int x = 0; x<4;x++){
-        if(x==0){
-            cardType = Card::spades;
-        }
-        else if(x==1){
-            cardType = Card::diamonds;
-        }
-        else if(x==2){
-            cardType = Card::clubs;
-        }
-        else{
-            cardType = Card::hearts;
-        }
-        for(int y = 0; y<13; y++){
-                Card *temp = new Card(y+1,cardType);
-                myCards[spot] = *temp;
-                //setbuf(stdout, 0);
-                //cout << myCards[spot].getRank() << "\n";
-                spot++;
-        }
 
+    player::player(string name){
+        myName = name;
+    };
+
+    string player::getName() const {
+        return myName;
+    };
+
+
+    void player::addCard(Card c){
+        if(myHand.size() <5){
+            myHand.push_back(c);
+        }
+        return;
+    };  //adds a card to the hand
+
+    //Adds card to the book
+    void player::bookCards(Card c1, Card c2){
+        myBook.push_back(c1);
+        myBook.push_back(c2);
+    };
+
+    //Does the player have the card c in her hand?
+    bool player::cardInHand(Card c)const{
+        vector <Card> ::const_iterator itr;
+        for(itr=myHand.begin();itr!=myHand.end();itr++){
+            if(c.getRank() == itr->getRank()){
+                return true;
+            }
+        }
+        return false;
     }
-};
 
-void Deck::shuffle(){
-    for(int x =0;x<100;x++){
-        int firstNum = rand()%51+0;
-        int secondNum = rand()%51+0;
-        Card firstCard = myCards[firstNum];
-        Card secondCard = myCards[secondNum];
-        myCards[secondNum] = firstCard;
-        myCards[firstNum] = secondCard;
-    }
-};
+    //Remove the card c from the hand and return it to the caller
+    Card player::removeCardFromHand(Card c){
+        vector <Card> ::iterator itr;
+        Card removalCard;
+        for(itr = myHand.begin();itr!=myHand.end();itr++){
+            if(itr->getRank() == c.getRank() && itr->sameSuitAs(c)){
+                removalCard = *itr;
+                myHand.erase(itr);
+                return removalCard;
+            };
+        }
+        return removalCard;
+    };
 
-Card Deck::dealCard(){
-    Card tmp;
-    if(myIndex == 52)
-        return tmp;
-    myIndex ++;
-    return myCards[myIndex-1];
-};
+    string player::showHand() const{};
+    string player::showBooks() const{};9
 
-int  Deck::size() const{
-    return SIZE - myIndex;
-};
-
-void Deck::toDeckString(){
-    for(int x =0; x<52; x++){
-        cout << myCards[x].toString() << "\n";
-    }
-}
+    int player::getHandSize() const{
+        return myHand.size();
+    };
+    int player::getBookSize() const{
+        return myBook.size();
+    };
